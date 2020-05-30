c26b0ce6050a9a17a245bfea4cca1dde7a8850ae
robson.ads.fatec@gmail.com <robson.code.it@gmail.com>
2020-05-30 20:10:28 -0300

    Commit inicial LivroDeNotas. Método modificador e de acesso, uso do this. Aula 16 Maio de 2020

---
+++ LivroDeNotas.java
@@ -0,0 +1,22 @@
+//frequentam o curso
+
+public class LivroDeNotas {
+
+    private String nomeDoCurso;
+
+    //vamos utilizaro construtor padrão
+    //presente na classe Object, e zera tudo
+
+    //Métodos de acesso
+    //consultar o valor de uma variavel de instância ou atributo
+    public String getNomeDoCurso() {
+
+      return nomeDoCurso;
+    }
+
+    //metodos modificadores
+    public void setNomeDoCurso(String nomeDoCurso) {
+
+      this.nomeDoCurso = nomeDoCurso;
+    }
+  }
